432b2d3763e53310e83f8bb85c42d447d299b735
YuHuaiChen <tp6m4cj96@gmail.com>
2015-03-16 21:47:55 +0800

    jia go

---
+++ CurveClient/src/client/CurveClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * @author dd
+ *
+ */
+public class CurveClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CurveServer/src/server/CurveServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author dd
+ *
+ */
+public class CurveServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
